a5fcea2c2fefe00e0b79affcba02e77cc69f779e
Aiden Isaia <aidenisaia@gmail.com>
2021-06-15 17:41:42 -0700

    Route and controller for retriving one and all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def one_product
+    recent = Product.last
+    render json: recent.as_json
+  end
+
+  def all_products
+    all = Product.all
+    render json: all.as_json
+  end
 end
